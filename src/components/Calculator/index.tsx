a68b810ed877ffbb47073bd0ed403c16ea04f7a7
Kenneth Wan <kenneth@madmadgroup.com>
2026-06-25 00:25:22 +0800

    chore: remove orphaned Calculator wrapper (superseded by SizerWorkspace tab)

--- src/components/Calculator/index.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import PositionSizerPanel from '@/components/Sizer/PositionSizerPanel';
-
-export default function Calculator() {
-  return <PositionSizerPanel chartMode="dialog" />;
-}
